bb157448ca601944004ad5e549a83837d6d75094
asobolevskiy <asobolevskiy@evapps.ru>
2026-08-14 15:58:28 +0300

    Fixed ViewFinderRect.Width/Height going stale after with-expressions

--- BarcodeScanner/Shared/Models/ViewFinderRect.shared.cs
+++ BarcodeScanner/Shared/Models/ViewFinderRect.shared.cs
@@ -6,8 +6,8 @@ public readonly record struct ViewFinderRect(
     float Right,
     float Bottom)
 {
-    public readonly float Width = Right - Left;
-    public readonly float Height = Bottom - Top;
+    public float Width => Right - Left;
+    public float Height => Bottom - Top;
 
     public static readonly ViewFinderRect Empty = new(0, 0, 0, 0);
 }
